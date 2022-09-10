5d2ad98040535c242d2783ab58a5a48ac9e035f6
benjamin kisenge <benkisenge03@gmail.com>
2022-09-10 23:57:22 +0200

    Create header markup

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Logo</h1>
+            <div>
+                <ul>
+                    <li>HOME</li>
+                    <li>MENU</li>
+                    <li>CONTACTS</li>
+                </ul>
+            </div>
+        </div>
+    )
+};
+
+export default Header;
