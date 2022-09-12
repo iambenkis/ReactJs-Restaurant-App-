440142fdfc3bc0d047fb90695a9ec8bdd04ef518
benjamin kisenge <benkisenge03@gmail.com>
2022-09-12 22:20:48 +0200

    Create commit text

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Header from './components/header';
 import Home from './components/homeView';
 import './App.css';
+import Menu from './components/menu';
+import Contact from './components/contact';
 
 function App() {
   return (
     <div>
       <Header />
       <Home />
+      <Menu />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/contact.js
@@ -0,0 +1,21 @@
+
+
+const Contact = (props) => (
+    <div className="contact">
+         <div className="menu-content">
+      <div className="menu-text">
+        <p className="try-text">TRY & DISCOVER</p>
+        <h3>CONTACTS</h3>
+        <p className="desc-p">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+          sed doeiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Utenim ad minim veniam, quis nostrud. Excepteur sint occaecat cupidatat non proident,
+          sunt in culpa. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+          doloremque laudantium, totam rem aperiam!
+        </p>
+      </div>
+    </div>
+    </div>
+  );
+
+  export default Contact;
